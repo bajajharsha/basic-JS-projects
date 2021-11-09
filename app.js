f8ba5abdcca8d42b1a118453c2dcaf7882299bbe
Harsha Bajaj <85387921+bajajharsha@users.noreply.github.com>
2021-11-09 12:55:29 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-const closedFace =document.querySelector('.closed')
-const openFace =document.querySelector('.open')
+const circle = document.querySelector('#circle')
 
-// add event listener
-
-closedFace.addEventListener('click', ()=>{     // event listeners adds click, load etc. 
-    if (openFace.classList.contains('open')) {   //if open face includes the class 'open' (jo woh karta hai)  then add the class active and closed face me se remove active
-        openFace.classList.add('active')
-        closedFace.classList.remove('active')
+circle.addEventListener('mouseenter', ()=>{
+    if (!circle.classList.contains('hover')){
+        circle.classList.add('hover');
     }
 })
 
-// ab open face change ho gaya into closed face. 
-
-openFace.addEventListener('click', ()=>{    //if closed face includes the class 'close' (jo woh karta hai)  then add the class active and open face me se remove active
-    if (closedFace.classList.contains('closed')) {
-        closedFace.classList.add('active')
-        openFace.classList.remove('active')
+circle.addEventListener('mouseleave', ()=>{
+    if (circle.classList.contains('hover')){
+        circle.classList.remove('hover');
     }
 })
+
